bda7bb38b91ec86427df3df6794f30f22346fc35
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-02-23 16:11:06 +0530

    Update manual.py

--- manual.py
+++ manual.py
@@ -1,2 +1,8 @@
 import TelePairNumber as TP
-for i in range (0, len(MAJOR_COLORS)):
+
+def printManual():
+  for i in range (0, len(MAJOR_COLORS)):
+    for j in range (0, len(MINOR_COLORS)):
+     pair_name= TP.color_pair_to_string(MAJOR_COLORS[i], MINOR_COLORS[j])
+     pair_value=TP. get_pair_number_from_color(MAJOR_COLORS[i], MINOR_COLORS[j])
+     print(pair_name, " : ", pair_value)
